f8455072b5a11bb968ed1959ed2c869d7025fe11
DADALA VENKATESH PRASAD <81640910+venky0101@users.noreply.github.com>
2021-12-29 15:59:12 +0530

    Create Insert a Node at the Tail of a Linked List.c

---
+++ Insert a Node at the Tail of a Linked List.c
@@ -0,0 +1,86 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+typedef struct SinglyLinkedListNode SinglyLinkedListNode;
+typedef struct SinglyLinkedList SinglyLinkedList;
+
+struct SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode* next;
+};
+
+struct SinglyLinkedList {
+    SinglyLinkedListNode* head;
+};
+
+SinglyLinkedListNode* create_singly_linked_list_node(int node_data) {
+    SinglyLinkedListNode* node = malloc(sizeof(SinglyLinkedListNode));
+
+    node->data = node_data;
+    node->next = NULL;
+
+    return node;
+}
+
+void print_singly_linked_list(SinglyLinkedListNode* node, char* sep, FILE* fptr) {
+    while (node) {
+        fprintf(fptr, "%d", node->data);
+
+        node = node->next;
+
+        if (node) {
+            fprintf(fptr, "%s", sep);
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtTail function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+    SinglyLinkedListNode *newnode;
+    SinglyLinkedListNode* temp=head;
+    newnode=(SinglyLinkedListNode*)malloc(sizeof(SinglyLinkedListNode));newnode->data=data;
+    newnode->next=NULL;
+    if (temp==NULL)
+    {
+        head=newnode;
+        return head; 
+    }
+    while(temp->next!=NULL)
+    {         
+    temp=temp->next;
+    }
+    temp->next=newnode;
+    temp->next=newnode;
+    newnode->next=NULL;
+    return head;
+
+}
+int main()
